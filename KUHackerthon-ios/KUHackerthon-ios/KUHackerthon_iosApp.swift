31d58de3c8cdb1ffc0a88b28675b5b03a56b800a
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-08-20 02:43:08 +0900

    [FEAT] KUhackerthon sandbox

---
+++ KUHackerthon-ios/KUHackerthon-ios/KUHackerthon_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KUHackerthon_iosApp.swift
+//  KUHackerthon-ios
+//
+//  Created by 양유진 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct KUHackerthon_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
